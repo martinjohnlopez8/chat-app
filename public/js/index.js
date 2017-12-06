a55d711ce99b6f3e37d06c256d0043a72806c838
Martin John R. Lopez <Tinjo@Martins-MacBook-Pro.local>
2017-12-06 16:08:42 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function ()  {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from: 'Martin',
-    text: 'Yep, that works'
-  });
 });
 
 socket.on('disconnect', function ()  {
