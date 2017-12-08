0519fd56bdde029534fd016bb1e57bdf216b21e3
Martin John R. Lopez <Tinjo@Martins-MacBook-Pro.local>
2017-12-08 21:12:16 +0800

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -33,11 +33,13 @@ socket.on('newLocationMessage', function (message) {
 $('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  var messageTextbox = $('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: $('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+    messageTextbox.val('')
   });
 });
 
@@ -47,13 +49,17 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
     console.log(position)
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   })
 });
