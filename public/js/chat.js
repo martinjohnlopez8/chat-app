f213885a39a32985f63afecb02bda277d80149eb
Martin John R. Lopez <Tinjo@Martins-MacBook-Pro.local>
2017-12-18 20:45:29 +0800

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -85,7 +85,6 @@ $('#message-form').on('submit', function (e) {
   var messageTextbox = $('[name=message]');
 
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function () {
     messageTextbox.val('')
